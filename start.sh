e119a977ab1279efa8483074adb0e21e6f51595b
showi <joachim.basmaison@gmail.com>
2016-06-01 22:17:29 +0200

    update docker

--- start.sh
+++ start.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/sh
 export PYTHONPATH="."
 mkdir -p /tmp/monu
-#/etc/init.d/mongodb start
-#echo "127.0.0.1 db" >> /etc/hosts
 python monu/script/install_data.py
-python monu/mdb/redux.py
-#python httpd
+python httpd
